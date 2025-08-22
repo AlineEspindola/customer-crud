c9b0c1c7bb07c236bb365d639823c1b100613a93
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2025-08-22 19:49:56 -0300

    style: Arruma largura dos inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,13 +4,15 @@ interface FormProps {}
 
 export default function Form(props: FormProps) {
   return (
-    <div className="mt-6 bg-white space-y-4">
-      <h2 className="text-lg font-semibold text-gray-800 mb-4 text-alig">Novo Contato</h2>
-      <Input text="Nome" value="Joaquim" />
-      <Input text="Empresa" value="PetLove" />
-      <Input text="Perfil do LinkedIn" value="LINK" />
-      <Input text="Último Contato" value="21/07/2025" />
-      <Input text="O que falar" value="Estudos Devops" />
+    <div className="mt-6 bg-white space-y-4 w-full">
+      <h2 className="text-lg font-semibold text-gray-800 mb-4">Novo Contato</h2>
+      <div className="flex flex-col space-y-4">
+        <Input text="Nome" value="Joaquim" />
+        <Input text="Empresa" value="PetLove" />
+        <Input text="Perfil do LinkedIn" value="LINK" />
+        <Input text="Último Contato" value="21/07/2025" />
+        <Input text="O que falar" value="Estudos Devops" />
+      </div>
     </div>
   );
 }
